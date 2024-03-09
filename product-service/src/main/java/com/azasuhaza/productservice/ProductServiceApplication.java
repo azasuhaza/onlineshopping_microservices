c7e913b016fbea402283ef7d710d901df89573bc
azasuhaza <azasuhaza@gmail.com>
2024-03-09 23:40:46 +0800

    fix: product repository is null

--- product-service/src/main/java/com/azasuhaza/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/azasuhaza/productservice/ProductServiceApplication.java
@@ -1,13 +1,38 @@
 package com.azasuhaza.productservice;
 
+import java.math.BigDecimal;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProductServiceApplication {
+import com.azasuhaza.productservice.model.Product;
+import com.azasuhaza.productservice.repos.ProductRepository;
 
+import lombok.RequiredArgsConstructor;
+
+@SpringBootApplication
+@RequiredArgsConstructor
+public class ProductServiceApplication implements CommandLineRunner {
+
+	private final ProductRepository prodRepo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		if(prodRepo.count() <1) {
+			Product product = new Product();
+			
+			product.setName("frozen croissants");
+			product.setDescription("butter croissants");
+			product.setPrice(BigDecimal.valueOf(15.99));
+			
+			prodRepo.save(product);
+		}
 	}
 
 }
