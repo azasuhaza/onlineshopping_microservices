04988b5687e615de300b487c224e57cae80bcbee
azasuhaza <azasuhaza@gmail.com>
2024-03-09 22:55:56 +0800

    fixes

--- product-service/src/main/java/com/azasuhaza/productservice/service/ProductService.java
+++ product-service/src/main/java/com/azasuhaza/productservice/service/ProductService.java
@@ -18,7 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ProductService {
 
-	private final ProductRepository prodRepo;
+	private ProductRepository prodRepo;
 	
 	public void createProduct(ProductRequest productRequest) {
 		
